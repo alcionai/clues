a8b70ab098bbcc547afda0701a42e2694fae2793
Keepers <ryanfkeepers@gmail.com>
2023-11-14 10:35:50 -0700

    add new/wrap/stackWith funcs for QoL error creation (#35)

--- err.go
+++ err.go
@@ -598,13 +598,28 @@ func New(msg string) *Err {
 	return toErr(nil, msg, nil)
 }
 
-// Wrap returns a  clues.Err with a new message wrapping the old error.
+// NewWC is equivalent to clues.New("msg").WithClues(ctx)
+func NewWC(ctx context.Context, msg string) *Err {
+	return toErr(nil, msg, nil).WithClues(ctx)
+}
+
+// Wrap returns a clues.Err with a new message wrapping the old error.
 func Wrap(err error, msg string) *Err {
 	if err == nil {
 		return nil
 	}
 
 	return toErr(err, msg, nil)
+}
+
+// WrapWC is equivalent to clues.Wrap(err, "msg").WithClues(ctx)
+// Wrap returns a clues.Err with a new message wrapping the old error.
+func WrapWC(ctx context.Context, err error, msg string) *Err {
+	if err == nil {
+		return nil
+	}
+
+	return Wrap(err, msg).WithClues(ctx)
 }
 
 // Stack returns the error as a clues.Err.  If additional errors are
@@ -629,6 +644,16 @@ func Stack(errs ...error) *Err {
 	}
 
 	return toStack(filtered[0], filtered[1:])
+}
+
+// StackWC is equivalent to clues.Stack(errs...).WithClues(ctx)
+func StackWC(ctx context.Context, errs ...error) *Err {
+	stack := Stack(errs...)
+	if stack == nil {
+		return nil
+	}
+
+	return stack.WithClues(ctx)
 }
 
 // ---------------------------------------------------------------------------
