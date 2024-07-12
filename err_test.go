5843f741578d3735a9b8e3dbf3bf95650a1fec46
Keepers <rkeepers@alcion.ai>
2024-07-12 15:54:32 -0600

    add StackWrap
    adds a StackWrap func, which is a quality of life shorthand replacing:
    clues.Stack(sentinel, clues.Wrap(err, "some message"))
    with:
    clues.StackWrap(sentinel, err, "some message")

--- err_test.go
+++ err_test.go
@@ -788,6 +788,13 @@ var testTable = []struct {
 		expectValues: msa{},
 	},
 	{
+		name:         "stackwrap",
+		err:          clues.StackWrap(target, sentinel, "wrap"),
+		expectMsg:    "target: wrap: sentinel",
+		expectLabels: msa{},
+		expectValues: msa{},
+	},
+	{
 		name:         "wrap two stack: top",
 		err:          clues.Wrap(clues.Stack(target, sentinel, other), "wrap"),
 		expectMsg:    "wrap: target: sentinel: other",
