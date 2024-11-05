9db2431bdf5c9482d4c0c1c8ed0f0ca66d0c1142
Keepers <ryanfkeepers@gmail.com>
2024-11-05 09:39:32 -0700

    reposition fmt and secrets (#62)
    In preparation for otel support, I'm repositioning some files and funcs.
    
    1. secrets is moving into `/clues/cecrets`. This move is for two
    reasons: 1/ reduce clues api surface area. 2/ isolate "secret" handling
    from general clues patterns.
    
    2. marshal() and normalize() are moving into
    `/clues/internal/stringify`. Marshal will get exposed with a Fmt command
    for other packages to use (this will be a requirement for otel attribute
    management).
    
    Outside of the introduction of the Fmt() func, no logical changes should
    have occurred in this PR. It's all movement/renaming.

--- datanode.go
+++ datanode.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"path"
-	"reflect"
 	"runtime"
 	"strings"
 
@@ -79,60 +78,6 @@ func (dn *dataNode) spawnDescendant() *dataNode {
 // ---------------------------------------------------------------------------
 // setters
 // ---------------------------------------------------------------------------
-
-// normalize ensures that the variadic of key-value pairs is even in length,
-// and then transforms that slice of values into a map[string]any, where all
-// keys are transformed to string using the marshal() func.
-func normalize(kvs ...any) map[string]any {
-	norm := map[string]any{}
-
-	for i := 0; i < len(kvs); i += 2 {
-		key := marshal(kvs[i], true)
-
-		var value any
-		if i+1 < len(kvs) {
-			value = marshal(kvs[i+1], true)
-		}
-
-		norm[key] = value
-	}
-
-	return norm
-}
-
-// marshal is the central marshalling handler for the entire package.  All
-// stringification of values comes down to this function.  Priority for
-// stringification follows this order:
-// 1. nil -> ""
-// 2. conceal all concealer interfaces
-// 3. flat string values
-// 4. string all stringer interfaces
-// 5. fmt.sprintf the rest
-func marshal(a any, conceal bool) string {
-	if a == nil {
-		return ""
-	}
-
-	// protect against nil pointer values with value-receiver funcs
-	rvo := reflect.ValueOf(a)
-	if rvo.Kind() == reflect.Ptr && rvo.IsNil() {
-		return ""
-	}
-
-	if as, ok := a.(Concealer); conceal && ok {
-		return as.Conceal()
-	}
-
-	if as, ok := a.(string); ok {
-		return as
-	}
-
-	if as, ok := a.(fmt.Stringer); ok {
-		return as.String()
-	}
-
-	return fmt.Sprintf("%+v", a)
-}
 
 // addValues adds all entries in the map to the dataNode's values.
 func (dn *dataNode) addValues(m map[string]any) *dataNode {
