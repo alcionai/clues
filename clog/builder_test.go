eeb81a076e47d902668f9c536a47a85cfbc9a949
Ashlie Martinez <ashlie.martinez@veeam.com>
2025-04-07 08:25:49 -0700

    Fix formatting and test name

--- clog/builder_test.go
+++ clog/builder_test.go
@@ -53,17 +53,17 @@ func TestBuilder(t *testing.T) {
 
 			bld.SkipCaller(1)
 
-		// ensure no collision between separate builders
-		// using the same ctx.
-		err := cluerr.New("an error").
-			With("fnords", "i have seen them").
-			Label("errLabel")
+			// ensure no collision between separate builders
+			// using the same ctx.
+			err := cluerr.New("an error").
+				With("fnords", "i have seen them").
+				Label("errLabel")
 
-		other := CtxErr(ctx, err)
-		assert.Empty(t, other.with)
-		assert.Empty(t, other.labels)
-		assert.Empty(t, other.comments)
-		assert.ErrorIs(t, other.err, err, cluerr.ToCore(err))
+			other := CtxErr(ctx, err)
+			assert.Empty(t, other.with)
+			assert.Empty(t, other.labels)
+			assert.Empty(t, other.comments)
+			assert.ErrorIs(t, other.err, err, cluerr.ToCore(err))
 
 			other.With("foo", "smarf")
 			assert.Contains(t, other.with, "foo")
