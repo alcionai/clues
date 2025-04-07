df2e52d9a51b6fa7da83115c874777df4d9c44aa
Dmitry Golushko <dmitry.golushko@veeam.com>
2025-04-07 19:58:24 +0200

    Map normalization for cluerr.WithMap (#137)
    Value passed to `cluerr.WithMap` was not normalized. 
    This PR introduces new `stringify.NormalizeMap` function which sync
    normalization implementation for maps with `clues.AddMap `

--- internal/stringify/fmt.go
+++ internal/stringify/fmt.go
@@ -104,3 +104,13 @@ func Normalize(kvs ...any) map[string]any {
 
 	return norm
 }
+
+func NormalizeMap[K comparable, V any](m map[K]V) map[string]any {
+	kvs := make([]any, 0, len(m)*2)
+
+	for k, v := range m {
+		kvs = append(kvs, k, v)
+	}
+
+	return Normalize(kvs...)
+}
