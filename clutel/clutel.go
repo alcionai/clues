57da5da1d560a40d66376c7275b5cff10bc56a71
Keepers <ryan.keepers@veeam.com>
2025-07-25 17:58:55 -0600

    add span opts to clutel
    1. move clues add and close span funcs to clutel as start and end span.
    2. add a span builder to clutel.

--- clutel/clutel.go
+++ clutel/clutel.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/alcionai/clues/internal/node"
 	"github.com/alcionai/clues/internal/stringify"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // AddToOTELHTTPLabeler adds key-value pairs to both the current
@@ -30,4 +31,100 @@ func AddToOTELHTTPLabeler(
 		node.DoNotAddToSpan(),
 		node.AddToOTELHTTPLabeler(ctx),
 	))
+}
+
+// GetSpan retrieves the current OpenTelemetry span from the context.
+func GetSpan(ctx context.Context) trace.Span {
+	return trace.SpanFromContext(ctx)
+}
+
+type spanBuilder struct {
+	kvs  map[string]any
+	opts []trace.SpanStartOption
+}
+
+// NewSpan produces a span builder that allows complete configuration and
+// attribution of the span before it gets started.
+func NewSpan() *spanBuilder {
+	return &spanBuilder{}
+}
+
+// WithAttrs adds attrs to the span.
+func (sb *spanBuilder) WithAttrs(kvs ...any) *spanBuilder {
+	if sb.kvs == nil {
+		sb.kvs = make(map[string]any)
+	}
+
+	// nc.AddValues(stringify.Normalize(kvs...))
+	sb.kvs = stringify.Normalize(kvs...)
+
+	return sb
+}
+
+// WithOpts configures the span with specific otel options.  Note that one
+// of the options can be `trace.WithAttributes()`; don't use that, use
+// spanBuilder.WithAttrs() instead.
+func (sb *spanBuilder) WithOpts(opts ...trace.SpanStartOption) *spanBuilder {
+	if sb.opts == nil {
+		sb.opts = make([]trace.SpanStartOption, 0, len(opts))
+	}
+
+	sb.opts = append(sb.opts, opts...)
+
+	return sb
+}
+
+// Start begins the span with the provided name and attaches it to the context.
+func (sb *spanBuilder) Start(
+	ctx context.Context,
+	name string,
+) context.Context {
+	nc := node.FromCtx(ctx)
+	ctx, spanned := nc.AddSpan(ctx, name, sb.opts...)
+
+	if len(sb.kvs) > 0 {
+		spanned.ID = name
+		spanned = spanned.AddValues(sb.kvs)
+	} else {
+		spanned = spanned.AppendToTree(name)
+	}
+
+	return node.EmbedInCtx(ctx, spanned)
+}
+
+// StartSpan stacks a clues node onto this context and uses the provided
+// name to generate an OTEL span name. StartSpan can be called without
+// adding attributes. Callers should always follow this addition with a
+// closing `defer clues.EndSpan(ctx)`.
+func StartSpan(
+	ctx context.Context,
+	name string,
+	kvs ...any,
+) context.Context {
+	nc := node.FromCtx(ctx)
+	if nc == nil {
+		return ctx
+	}
+
+	var spanned *node.Node
+
+	if len(kvs) > 0 {
+		ctx, spanned = nc.AddSpan(ctx, name)
+		spanned.ID = name
+		spanned = spanned.AddValues(stringify.Normalize(kvs...))
+	} else {
+		ctx, spanned = nc.AddSpan(ctx, name)
+		spanned = spanned.AppendToTree(name)
+	}
+
+	return node.EmbedInCtx(ctx, spanned)
+}
+
+// EndSpan closes the current span in the clues node.  Should only be called
+// following a `clues.AddSpan()` call.
+func EndSpan(ctx context.Context) context.Context {
+	return node.EmbedInCtx(
+		ctx,
+		node.FromCtx(ctx).CloseSpan(ctx),
+	)
 }
